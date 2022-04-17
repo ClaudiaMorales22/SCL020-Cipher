71e13623d4b9a3f6dcc960c64c0d416bdfbbae52
ClaudiaMorales22 <claudia.extreme2204@gmail.com>
2022-04-16 21:05:01 -0400

    Funcion conversion a mayusculas del texto y funcion del boton cifrar

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+
+document.getElementById("message").addEventListener("keyup", myFunction);
+function myFunction (){
+    var x = document.getElementById("message");
+    x.value = x.value.toUpperCase();
+}
+
+
+//Cipher 
+
+document.getElementById("cifrarmsj").addEventListener("click", function () {
+let messageBox1 = document.getElementById("message").value; 
+let shift = parseInt(document.getElementById("desplazamientoci").value);
+let result =cipher.encode(messageBox1, shift);
+document.getElementById("resultado").value = result;
+});
+
+
+
