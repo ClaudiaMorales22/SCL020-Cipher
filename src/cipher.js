fc9a1a65a769793e7a5f23c6e5dada58a6150c2b
ClaudiaMorales22 <claudia.extreme2204@gmail.com>
2022-04-16 21:03:23 -0400

    Funcion para codificar y decodificar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,39 @@
 const cipher = {
-  // ...
+
+  encode (messageBox1, shift) { 
+    let result = "", code;
+    for (let i=0; i<messageBox1.length;i++){
+      if (messageBox1.charCodeAt(i)>=65 && messageBox1.charCodeAt(i)<=90){
+        code = (((messageBox1.charCodeAt(i)-65)+shift)%26)+65;
+      }
+      else if (messageBox1.charCodeAt(i) >= 33 && messageBox1.charCodeAt(i) <= 66) {
+        code = (((messageBox1.charCodeAt(i) + 33) - shift) % 33) + 33; 
+      }
+      else if (messageBox1.charCodeAt(i)===32){
+        code = 32;
+      }
+      result+=String.fromCharCode(code);
+    }
+    return result;
+  },
+
+
+  decode (messageBox2, shift2) {
+    let result = "", code;
+    for (let i=0; i<messageBox2.length;i++){ 
+      if (messageBox2.charCodeAt(i)>=65 && messageBox2.charCodeAt(i)<=90){
+        code = (((messageBox2.charCodeAt(i)+65)-shift2)%26)+65;
+      }
+     else if (messageBox2.charCodeAt(i) >= 33 && messageBox2.charCodeAt(i) <= 66) {
+        code = (((messageBox2.charCodeAt(i) + 33) + shift2) % 33) + 33;
+      }
+      else if (messageBox2.charCodeAt(i)===32){
+        code = 32;
+      }
+      result+=String.fromCharCode(code);
+    }
+    return result;
+  },
 };
 
 export default cipher;
